31db486442eadff8f405ae35e4050a9e23557ad2
Michael Carroll <mcarroll@diversecomputing.com>
2024-01-23 11:15:23 -0500

    working on pages

--- src/main/java/com/diversecomputing/demoproject/controllers/HomeController.java
+++ src/main/java/com/diversecomputing/demoproject/controllers/HomeController.java
@@ -1,13 +1,28 @@
 package com.diversecomputing.demoproject.controllers;
 
+import com.diversecomputing.demoproject.movie.Movie;
+import com.diversecomputing.demoproject.movie.MovieService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
+    final MovieService movieService;
+
+    public HomeController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
     @GetMapping("/")
-    public String home(){
-        return "Home";
+    public String home(Model model){
+        final List<Movie> movies = movieService.getMovies();
+
+        model.addAttribute("movies", movies);
+
+        return "home";
     }
 }
---
+++ src/main/java/com/diversecomputing/demoproject/controllers/SearchController.java
@@ -0,0 +1,34 @@
+package com.diversecomputing.demoproject.controllers;
+
+import com.diversecomputing.demoproject.movie.Movie;
+import com.diversecomputing.demoproject.movie.MovieDao;
+import com.diversecomputing.demoproject.movie.MovieService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class SearchController {
+    final MovieService movieService;
+
+    public SearchController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/search")
+    public String searchPage(){
+        return "search";
+    }
+    @GetMapping("/search/{id}")
+    public String search(@ModelAttribute("movieSearchFormData") Movie formData, Model model){
+        Movie mov = movieService.getMovie(formData.id());
+        model.addAttribute("movie", mov);
+
+        return "search";
+
+    }
+}
