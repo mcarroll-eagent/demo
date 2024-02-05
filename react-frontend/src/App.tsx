606ccf2fc5624ae6c06db12af93ac9b45a774ebe
Michael Carroll <mcarroll@diversecomputing.com>
2024-02-05 11:10:08 -0500

    Home page + favicon and title

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -4,24 +4,33 @@ import AllMovies from "./components/pages/AllMovies.tsx";
 import styled from "styled-components";
 import Search from "./components/pages/Search.tsx";
 import DynamicSearch from "./components/pages/DynamicSearch.tsx";
+import { Helmet } from "react-helmet";
+import Home from "./components/pages/Home.tsx";
 
 const AppWrapper = styled.div`
-    background-color: #fefefe;
+    background-color: #121212;
     height: 100vh;
     width: 100%;
 `
 
 function App(){
     return (
+        <>
+            <Helmet>
+                <title>My Movie with React</title>
+                <link rel={'icon'} type="image/png" href={"./MyMovieIcon.png"}/>
+            </Helmet>
+
         <AppWrapper>
             <Navbar/>
             <Routes>
+                <Route path={"/"} element={<Home/>}/>
                 <Route path={"/allmovies"} element={<AllMovies/>}/>
                 <Route path={"/search"} element={<Search/>}/>
                 <Route path={"/searchv2"} element={<DynamicSearch/>}/>
             </Routes>
         </AppWrapper>
-
+        </>
     );
 }
 
---
+++ react-frontend/src/components/pages/Home.tsx
@@ -0,0 +1,67 @@
+import styled, {keyframes} from "styled-components";
+
+const HomeContainer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 75px);
+    background-color: #121212;
+    overflow: hidden;
+`
+const Glow = keyframes`
+    0%
+    {
+        opacity: 0;
+    }
+    
+    100%{
+        opacity: 1;
+    }
+`
+const Logo = styled.p`
+    color: rgb(137, 206, 255);
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    font-size: 80px;
+    font-family: "Kohinoor Bangla", serif;
+    font-weight: bold;
+    text-decoration: none;
+    background: linear-gradient(#e2f6ff, dodgerblue);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-box-reflect: below 1px -webkit-linear-gradient(transparent, rgba(0, 0, 0, 0.37));
+    line-height: 58px;
+    margin: 0;
+    
+    
+    animation: ${Glow} 2s linear 1;
+`
+
+const Text = styled.p`
+    color: rgb(255, 255, 255);
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    font-size: 50px;
+    font-family: "Kohinoor Bangla", serif;
+    font-weight: bold;
+    text-decoration: none;
+    margin: 0;
+`
+
+function Home()
+{
+    return(
+        <>
+            <HomeContainer>
+                <Text>Welcome to</Text>
+                <Logo>My Movie.</Logo>
+            </HomeContainer>
+
+
+        </>
+    )
+}
+
+export default Home;
